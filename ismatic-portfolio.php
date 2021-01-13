4b1504aa320c8c4f0c0d1f85ebbe3e6b8ea1f49b
Terver Ugbe <32528384+Dennisruso@users.noreply.github.com>
2021-01-13 20:33:46 +0100

    portfolio and about-us structure

--- ismatic-portfolio.php
+++ ismatic-portfolio.php
@@ -3,138 +3,74 @@ $web_title = "Portfolio";
 include('header.php');
 ?>
 
-   <!-- ========== Page Title ========== -->
+       
+        <!-- ========== Featured Projects ========== -->
 
-   <header style="margin-top: 70px;" class="page-title pt-large pt-light pt-parallax pt-plax-lg-light" data-stellar-background-ratio="0.4">
-          <div class="container">
-              <div class="row">
-
-                <div class="col-sm-6">
-                  <h1>Portfolio</h1>
-                  <span class="subheading">Some of our work</span>
-                </div>
-                <ol class="col-sm-6 text-right breadcrumb">
-                  <li><a href="#">Home</a></li>
-                  <li><a href="#">Elements</a></li>
-                  <li class="active">Portfolio</li>
-                </ol>
-
-              </div>
-            </div>
-        </header>
-
-
-        <!-- ========== Portfolio Full Width - 3 Columns ========== -->
-
-        <section class="container-fluid portfolio-layout portfolio-columns-fw">
+        <div class="container-fluid ft-hover-item">
           <div class="row">
-            <header class="sec-heading">
-              <h2>Some of our project</h2>
-              <span class="subheading">Lorem ipsum dolor sit amet consectetur adipisicing</span>
-            </header>
+            
+            <!-- Item 1 -->
+            <div class="ft-item ft-1 col-md-6">
+              <div class="bg-overlay">
+
+                <div class="content-wrapper">
+                  <a href="ismatic-portfolio.php"><h3>Project Name</h3></a>
+                  <p>Description about the project,<br> more content with few words</p>
+                  <a href="ismatic-portfolio.php" class="btn-ghost btn-ghost-light ft-button">More Projects</a>
+                </div>
+
+              </div><!-- / .bg-overlay -->
+            </div><!-- / .ft-item -->
+
+            <!-- Item 2 -->
+            <div class="ft-item ft-2 col-md-6">
+              <div class="bg-overlay">
+
+                <div class="content-wrapper">
+                  <a href="ismatic-portfolio.php"><h3>Project Name</h3></a>
+                  <p>Description about the project,<br> more content with few words</p>
+                  <a href="ismatic-portfolio.php" class="btn-ghost btn-ghost-light ft-button">More Projects</a>
+                </div>
+
+              </div><!-- / .bg-overlay -->
+            </div><!-- / .ft-item -->
+
+            
+
           </div><!-- / .row -->
-          
-          <!-- Filters -->
-          <div class="row">
-            <ul id="pfolio-filters" class="portfolio-filters">
-              <li class="active"><a href="#" data-filter="*">All</a></li>
-              <li><a href="#" data-filter=".webdesing">Webdesign</a></li>
-              <li><a href="#" data-filter=".print">Print</a></li>
-              <li><a href="#" data-filter=".photo">Photography</a></li>
-            </ul>
-          </div>
 
           <div class="row">
-            <div id="pfolio">
+            
+            <!-- Item 1 -->
+            <div class="ft-item ft-3 col-md-6">
+              <div class="bg-overlay">
 
-              <!-- Item 1 -->
-              <div class="col-md-4 portfolio-item print">
-                <div class="p-wrapper hover-default">
-                  <img src="http://placehold.it/990x650/aaa/ccc" alt="Project Example">
-                  <div class="p-hover">
-                    <div class="p-content">
-                      <h4>Project Name</h4>
-                      <h6 class="subheading">Sub heading for the project</h6>
-                    </div>
-                  </div>
-                  <a href="portfolio-single-1.html" class="open-btn"><i class="fa fa-expand"></i></a>
+                <div class="content-wrapper">
+                  <a href="ismatic-portfolio.php"><h3>Project Name</h3></a>
+                  <p>Description about the project,<br> more content with few words</p>
+                  <a href="ismatic-portfolio.php" class="btn-ghost btn-ghost-light ft-button">More Projects</a>
                 </div>
-              </div><!-- / .portfolio-item -->
 
-              <!-- Item 2 -->
-              <div class="col-md-4 portfolio-item webdesing photo">
-                <div class="p-wrapper hover-default">
-                  <img src="http://placehold.it/990x650" alt="Project Example">
-                  <div class="p-hover">
-                    <div class="p-content">
-                      <h4>Project Name</h4>
-                      <h6 class="subheading">Sub heading for the project</h6>
-                    </div>
-                  </div>
-                  <a href="portfolio-single-1.html" class="open-btn"><i class="fa fa-expand"></i></a>
+              </div><!-- / .bg-overlay -->
+            </div><!-- / .ft-item -->
+
+            <!-- Item 2 -->
+            <div class="ft-item ft-4 col-md-6">
+              <div class="bg-overlay">
+
+                <div class="content-wrapper">
+                  <a href="ismatic-portfolio.php"><h3>Project Name</h3></a>
+                  <p>Description about the project,<br> more content with few words</p>
+                  <a href="ismatic-portfolio.php" class="btn-ghost btn-ghost-light ft-button">More Projects</a>
                 </div>
-              </div><!-- / .portfolio-item -->
 
-              <!-- Item 3 -->
-              <div class="col-md-4 portfolio-item photo print">
-                <div class="p-wrapper hover-default">
-                  <img src="http://placehold.it/990x650/aaa/ccc" alt="Project Example">
-                  <div class="p-hover">
-                    <div class="p-content">
-                      <h4>Project Name</h4>
-                      <h6 class="subheading">Sub heading for the project</h6>
-                    </div>
-                  </div>
-                  <a href="portfolio-single-1.html" class="open-btn"><i class="fa fa-expand"></i></a>
-                </div>
-              </div><!-- / .portfolio-item -->
+              </div><!-- / .bg-overlay -->
+            </div><!-- / .ft-item -->
 
-              <!-- Item 4 -->
-              <div class="col-md-4 portfolio-item webdesing print">
-                <div class="p-wrapper hover-default">
-                  <img src="http://placehold.it/990x650" alt="Project Example">
-                  <div class="p-hover">
-                    <div class="p-content">
-                      <h4>Project Name</h4>
-                      <h6 class="subheading">Sub heading for the project</h6>
-                    </div>
-                  </div>
-                  <a href="portfolio-single-1.html" class="open-btn"><i class="fa fa-expand"></i></a>
-                </div>
-              </div><!-- / .portfolio-item -->
+            
 
-              <!-- Item 5 -->
-              <div class="col-md-4 portfolio-item webdesing">
-                <div class="p-wrapper hover-default">
-                  <img src="http://placehold.it/990x650/aaa/ccc" alt="Project Example">
-                  <div class="p-hover">
-                    <div class="p-content">
-                      <h4>Project Name</h4>
-                      <h6 class="subheading">Sub heading for the project</h6>
-                    </div>
-                  </div>
-                  <a href="portfolio-single-1.html" class="open-btn"><i class="fa fa-expand"></i></a>
-                </div>
-              </div><!-- / .portfolio-item -->
-
-              <!-- Item 6 -->
-              <div class="col-md-4 portfolio-item photo print webdesing">
-                <div class="p-wrapper hover-default">
-                  <img src="http://placehold.it/990x650" alt="Project Example">
-                  <div class="p-hover">
-                    <div class="p-content">
-                      <h4>Project Name</h4>
-                      <h6 class="subheading">Sub heading for the project</h6>
-                    </div>
-                  </div>
-                  <a href="portfolio-single-1.html" class="open-btn"><i class="fa fa-expand"></i></a>
-                </div>
-              </div><!-- / .portfolio-item -->
-
-            </div><!-- / #pfolio -->
           </div><!-- / .row -->
-        </section><!-- / .portfolio-columns-fw -->
-
+        </div><!-- / .container-fluid -->
 
 
 <?php 
