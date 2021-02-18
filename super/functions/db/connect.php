3e9c3c574abe894b909aebfa20576c492da565a3
Pinza studio <67124011+dev-pinza@users.noreply.github.com>
2021-02-18 01:11:23 +0100

    READY TO DEPLOY FOR BETA VERSION

--- super/functions/db/connect.php
+++ super/functions/db/connect.php
@@ -1,17 +1,17 @@
 <?php
 // PRODUCTION
-// define('DB_SERVER', 'localhost');
-// define('DB_USERNAME', 'ismaticc_admin');
-// define('DB_PASSWORD', 'w&HXs(ZTeq}r');
-// define('DB_CHARSET', 'utf8');
-// define('DB_NAME', 'ismaticc_website');
-// test
-// DEV
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'bfslcom_apply');
-define('DB_PASSWORD', 'Apply633#@');
+define('DB_USERNAME', 'ismaticc_admin');
+define('DB_PASSWORD', 'w&HXs(ZTeq}r');
 define('DB_CHARSET', 'utf8');
 define('DB_NAME', 'ismaticc_website');
+// test
+// DEV
+// define('DB_SERVER', 'localhost');
+// define('DB_USERNAME', 'bfslcom_apply');
+// define('DB_PASSWORD', 'Apply633#@');
+// define('DB_CHARSET', 'utf8');
+// define('DB_NAME', 'ismaticc_website');
 
 $connection = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
